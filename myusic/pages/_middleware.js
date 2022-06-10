6b2ca38fd1e7376c6fa732629a38557661d640b3
KKsimform <kishan.k@simformsolutions.com>
2022-06-10 16:46:14 +0530

    remove useRouter

--- myusic/pages/_middleware.js
+++ myusic/pages/_middleware.js
@@ -1,5 +1,4 @@
-// import { NextResponse } from 'next/server'
-import { useRouter } from 'next/router'
+import { NextResponse } from 'next/server'
 
 const signedinPages = ['/', '/playlist', '/library']
 
@@ -10,8 +9,7 @@ export default function middleware(req) {
   if (signedinPages.find((p) => p === req.nextUrl.pathname)) {
     const token = req.cookies.MYUSIC_ACCESS_TOKEN
     if (!token) {
-      // return NextResponse.redirect('/signin')
-      route.push('/signin')
+      return NextResponse.redirect('/signin')
     }
   }
 }
