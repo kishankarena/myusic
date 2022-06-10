4884b7e55491fa7608762d62815edc2912ab8a2d
KKsimform <kishan.k@simformsolutions.com>
2022-06-10 16:43:40 +0530

    added useRouter hook

--- myusic/pages/_middleware.js
+++ myusic/pages/_middleware.js
@@ -1,14 +1,17 @@
-import { NextResponse } from 'next/server'
+// import { NextResponse } from 'next/server'
+import { useRouter } from 'next/router'
 
 const signedinPages = ['/', '/playlist', '/library']
 
 export default function middleware(req) {
   //   const url = req.nextUrl.clone();
+  const route = useRouter()
 
   if (signedinPages.find((p) => p === req.nextUrl.pathname)) {
     const token = req.cookies.MYUSIC_ACCESS_TOKEN
     if (!token) {
-      return NextResponse.redirect('/signin')
+      // return NextResponse.redirect('/signin')
+      route.push('/signin')
     }
   }
 }
